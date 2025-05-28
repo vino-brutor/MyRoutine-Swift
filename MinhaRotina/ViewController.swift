719f842db0f36d70281bca1bebbe10fdab6b5f6e
vitor <viktorbruno2006@gmail.com>
2025-05-28 15:34:28 -0300

    Initial Commit

---
+++ MinhaRotina/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MinhaRotina
+//
+//  Created by Vítor Bruno on 28/05/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
